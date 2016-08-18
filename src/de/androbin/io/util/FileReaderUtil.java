09cd1af8f39fcaf86bc58d43b28f28069ac1c5c4
Androbin <robin.richtsfeld@gmail.com>
2016-08-18 15:14:55 +0200

    Update and rename FileUtil.java to FileReaderUtil.java

--- src/de/androbin/io/util/FileReaderUtil.java
+++ src/de/androbin/io/util/FileReaderUtil.java
@@ -3,32 +3,11 @@ package de.androbin.io.util;
 import java.io.*;
 import java.util.*;
 import java.util.function.*;
-import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 
-public final class FileUtil
+public final class FileReaderUtil
 {
-	private FileUtil()
+	private FileReaderUtil()
 	{
-	}
-	
-	public static File askForFileOpening( final File dir, final FileFilter filter )
-	{
-		final JFileChooser jfc = createFileChooser( dir, filter );
-		return jfc.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
-	}
-	
-	public static File askForFileSaving( final File dir, final FileFilter filter )
-	{
-		final JFileChooser jfc = createFileChooser( dir, filter );
-		return jfc.showSaveDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
-	}
-	
-	public static JFileChooser createFileChooser( final File dir, final FileFilter filter )
-	{
-		final JFileChooser jfc = new JFileChooser( dir );
-		jfc.setFileFilter( filter );
-		return jfc;
 	}
 	
 	public static <T> T loadFile( final File file, final Function<FileInputStream, T> f )
