e72b614add43b56b5e1478e93da75895852fb6dc
Androbin <robin.richtsfeld@gmail.com>
2017-06-06 00:18:41 +0200

    Update Formatting

--- src/de/androbin/io/util/FileChooserUtil.java
+++ src/de/androbin/io/util/FileChooserUtil.java
@@ -4,51 +4,42 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 
-public final class FileChooserUtil
-{
-	private FileChooserUtil()
-	{
-	}
-	
-	public static File askForDirectoryOpening( final File dir )
-	{
-		final JFileChooser jfc = createDirectoryChooser( dir );
-		return openDialog( jfc );
-	}
-	
-	public static File askForFileOpening( final File dir, final FileFilter filter )
-	{
-		final JFileChooser jfc = createFileChooser( dir, filter );
-		return openDialog( jfc );
-	}
-	
-	public static File askForFileSaving( final File dir, final FileFilter filter )
-	{
-		final JFileChooser jfc = createFileChooser( dir, filter );
-		return saveDialog( jfc );
-	}
-	
-	public static JFileChooser createDirectoryChooser( final File dir )
-	{
-		final JFileChooser jfc = new JFileChooser( dir );
-		jfc.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
-		return jfc;
-	}
-	
-	public static JFileChooser createFileChooser( final File dir, final FileFilter filter )
-	{
-		final JFileChooser jfc = new JFileChooser( dir );
-		jfc.setFileFilter( filter );
-		return jfc;
-	}
-	
-	private static File openDialog( final JFileChooser jfc )
-	{
-		return jfc.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
-	}
-	
-	private static File saveDialog( final JFileChooser jfc )
-	{
-		return jfc.showSaveDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
-	}
+public final class FileChooserUtil {
+  private FileChooserUtil() {
+  }
+  
+  public static File askForDirectoryOpening( final File dir ) {
+    final JFileChooser jfc = createDirectoryChooser( dir );
+    return openDialog( jfc );
+  }
+  
+  public static File askForFileOpening( final File dir, final FileFilter filter ) {
+    final JFileChooser jfc = createFileChooser( dir, filter );
+    return openDialog( jfc );
+  }
+  
+  public static File askForFileSaving( final File dir, final FileFilter filter ) {
+    final JFileChooser jfc = createFileChooser( dir, filter );
+    return saveDialog( jfc );
+  }
+  
+  public static JFileChooser createDirectoryChooser( final File dir ) {
+    final JFileChooser jfc = new JFileChooser( dir );
+    jfc.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
+    return jfc;
+  }
+  
+  public static JFileChooser createFileChooser( final File dir, final FileFilter filter ) {
+    final JFileChooser jfc = new JFileChooser( dir );
+    jfc.setFileFilter( filter );
+    return jfc;
+  }
+  
+  private static File openDialog( final JFileChooser jfc ) {
+    return jfc.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
+  }
+  
+  private static File saveDialog( final JFileChooser jfc ) {
+    return jfc.showSaveDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
+  }
 }
--- src/de/androbin/io/util/FileReaderUtil.java
+++ src/de/androbin/io/util/FileReaderUtil.java
@@ -5,57 +5,45 @@ import java.net.*;
 import java.util.*;
 import java.util.function.*;
 
-public final class FileReaderUtil
-{
-	private FileReaderUtil()
-	{
-	}
-	
-	public static <T> T loadFile( final File file, final Function<FileInputStream, T> f )
-	{
-		try ( final FileInputStream stream = new FileInputStream( file ) )
-		{
-			return f.apply( stream );
-		}
-		catch ( final IOException e )
-		{
-			return null;
-		}
-	}
-	
-	public static String readFile( final File file ) throws IOException
-	{
-		try ( final Reader reader = new FileReader( file ) )
-		{
-			return read( reader );
-		}
-	}
-	
-	public static String read( final URL url ) throws IOException
-	{
-		return read( url.openStream() );
-	}
-	
-	public static String read( final InputStream stream ) throws IOException
-	{
-		return read( new InputStreamReader( stream ) );
-	}
-	
-	public static String read( final Reader reader ) throws IOException
-	{
-		return read( new BufferedReader( reader ) );
-	}
-	
-	public static String read( final BufferedReader reader ) throws IOException
-	{
-		final StringJoiner joiner = new StringJoiner( System.lineSeparator() );
-		read( reader, line -> joiner.add( line ) );
-		return joiner.toString();
-	}
-	
-	public static void read( final BufferedReader reader, final Consumer<String> consumer ) throws IOException
-	{
-		reader.lines().forEachOrdered( consumer );
-		reader.close();
-	}
+public final class FileReaderUtil {
+  private FileReaderUtil() {
+  }
+  
+  public static <T> T loadFile( final File file, final Function<FileInputStream, T> f ) {
+    try ( final FileInputStream stream = new FileInputStream( file ) ) {
+      return f.apply( stream );
+    } catch ( final IOException e ) {
+      return null;
+    }
+  }
+  
+  public static String readFile( final File file ) throws IOException {
+    try ( final Reader reader = new FileReader( file ) ) {
+      return read( reader );
+    }
+  }
+  
+  public static String read( final URL url ) throws IOException {
+    return read( url.openStream() );
+  }
+  
+  public static String read( final InputStream stream ) throws IOException {
+    return read( new InputStreamReader( stream ) );
+  }
+  
+  public static String read( final Reader reader ) throws IOException {
+    return read( new BufferedReader( reader ) );
+  }
+  
+  public static String read( final BufferedReader reader ) throws IOException {
+    final StringJoiner joiner = new StringJoiner( System.lineSeparator() );
+    read( reader, line -> joiner.add( line ) );
+    return joiner.toString();
+  }
+  
+  public static void read( final BufferedReader reader, final Consumer<String> consumer )
+      throws IOException {
+    reader.lines().forEachOrdered( consumer );
+    reader.close();
+  }
 }
