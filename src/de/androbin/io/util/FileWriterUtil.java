31db8fb0df4e5ad39f7f67996049f2dfc0fa6962
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-06-18 01:31:12 +0200

    Allocate file implicitly

--- src/de/androbin/io/util/FileWriterUtil.java
+++ src/de/androbin/io/util/FileWriterUtil.java
@@ -8,22 +8,13 @@ public final class FileWriterUtil {
   private FileWriterUtil() {
   }
   
-  private static void allocate( final Path file ) throws IOException {
-    Files.createDirectories( file.getParent() );
-    
-    try {
-      Files.createFile( file );
-    } catch ( final FileAlreadyExistsException ignore ) {
-    }
-  }
-  
   public static void writeFile( final Path file, final String content ) throws IOException {
     writeFile( file, writer -> writer.write( content ) );
   }
   
   public static void writeFile( final Path file,
       final DirtyConsumer<BufferedWriter, IOException> consumer ) throws IOException {
-    allocate( file );
+    Files.createDirectories( file.getParent() );
     
     try ( final BufferedWriter writer = Files.newBufferedWriter( file ) ) {
       consumer.accept( writer );
