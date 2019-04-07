9544d8f6f2626ef6503a9c40875bf5042103db12
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2019-04-07 19:23:16 +0200

    Return Optional<File> from FileChooser

--- src/de/androbin/io/util/FileChooserUtil.java
+++ src/de/androbin/io/util/FileChooserUtil.java
@@ -1,6 +1,7 @@
 package de.androbin.io.util;
 
 import java.io.*;
+import java.util.*;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 
@@ -20,27 +21,31 @@ public final class FileChooserUtil {
     return jfc;
   }
   
-  private static File openDialog( final JFileChooser jfc ) {
-    return jfc.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
+  private static Optional<File> openDialog( final JFileChooser jfc ) {
+    return jfc.showOpenDialog( null ) == JFileChooser.APPROVE_OPTION
+        ? Optional.of( jfc.getSelectedFile() )
+        : Optional.empty();
   }
   
-  public static File openDirectory( final File dir ) {
+  public static Optional<File> openDirectory( final File dir ) {
     return openDialog( createDirectoryChooser( dir ) );
   }
   
-  public static File openFile( final File dir, final FileFilter filter ) {
+  public static Optional<File> openFile( final File dir, final FileFilter filter ) {
     return openDialog( createFileChooser( dir, filter ) );
   }
   
-  private static File saveDialog( final JFileChooser jfc ) {
-    return jfc.showSaveDialog( null ) == JFileChooser.APPROVE_OPTION ? jfc.getSelectedFile() : null;
+  private static Optional<File> saveDialog( final JFileChooser jfc ) {
+    return jfc.showSaveDialog( null ) == JFileChooser.APPROVE_OPTION
+        ? Optional.of( jfc.getSelectedFile() )
+        : Optional.empty();
   }
   
-  public static File saveDirectory( final File dir ) {
+  public static Optional<File> saveDirectory( final File dir ) {
     return saveDialog( createDirectoryChooser( dir ) );
   }
   
-  public static File saveFile( final File dir, final FileFilter filter ) {
+  public static Optional<File> saveFile( final File dir, final FileFilter filter ) {
     return saveDialog( createFileChooser( dir, filter ) );
   }
 }
